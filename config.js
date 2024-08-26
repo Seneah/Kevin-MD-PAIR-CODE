c6c5189e39bc8c0f8283bcba296cbf05ef88612c
Seneah <chandrikadissanayake192@gmail.com>
2024-08-26 08:18:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,11 +6,11 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-IMG: process.env.IMG=`https://telegra.ph/file/d8279f4ca5da23bda7da4.jpg`,
-CAPTION: process.env.CAPTION=`*𝐁𝐇𝐀𝐒𝐇𝐈-𝐌𝐃 𝐒𝐄𝐒𝐒𝐈𝐎𝐍-𝐈𝐃*\n\n_🪄 ᴅᴏɴ'ᴛ ꜱʜᴀʀᴇ ʏᴏᴜʀ ꜱᴇꜱꜱɪᴏɴ ɪᴅ ᴡɪᴛʜ ᴀɴʏᴏɴᴇ_`,
+IMG: process.env.IMG=`https://telegra.ph/file/3c64b5608dd82d33dabe8.jpg`,
+CAPTION: process.env.CAPTION=`*KEVIN-Md 𝐒𝐄𝐒𝐒𝐈𝐎𝐍-𝐈𝐃*\n\n_🪄 ᴅᴏɴ'ᴛ ꜱʜᴀʀᴇ ʏᴏᴜʀ ꜱᴇꜱꜱɪᴏɴ ɪᴅ ᴡɪᴛʜ ᴀɴʏᴏɴᴇ_`,
 //----------------------------------------------------------------------------------------
 // මෙතනය ඔයා mega එකෙ Account එකක් හදලා එකෙ Email පාස් දාන්න ඔනේ මෙක වැඩ කරන්නෙනැතුව ගියොත් අවුල එන්නෙ ඔකෙ.
-EMAIL: process.env.EMAIL=`musicwow163@gmail.com`,
-PASS: process.env.PASS=`v22@v22@v22`,
+EMAIL: process.env.EMAIL=`pathumamantha2008@gmail.com`,
+PASS: process.env.PASS=`f4w.hmK*_bhWSKB`,
     
 };
